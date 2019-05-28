eaa86f725589e31caddbfc847731f95e760f7703
gujitao001 <gujitao001@ke.com>
2019-05-28 16:15:07 +0800

    解决编译错误

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo', 
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // counter didn't reset back to zero; the application is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter 1dddddd'),
     );
   }
   
